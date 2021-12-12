2a32070abccc14a9feef7ee14e263658af0d149b
Feralwater <lineviktoriya@gmail.com>
2021-12-12 20:30:31 +0300

    [task-7]: add loader to doctor`s resolutions

--- src/pages/Resolutions/ResolutionsForDoctor.tsx
+++ src/pages/Resolutions/ResolutionsForDoctor.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { format } from 'date-fns';
+import Loader from 'react-loader-spinner';
 import DoctorNavigatePanel from '../../components/NavigatePanel/DoctorNavigatePanel';
 import dictionary from '../dictionary/pagesDictionary';
 import { columnsNames, resolutionsOnPage, visitDate } from './constants';
@@ -9,6 +10,7 @@ import { useAppDispatch, useAppSelector } from '../../hooks';
 import { resolutions } from '../../redux/actions';
 import ResolutionsPaginate from './ResolutionsPaginate';
 import { selectResolutions } from '../../redux/reducers';
+import { colors } from '../../components';
 
 const ResolutionsForDoctor = () => {
   const dispatch = useAppDispatch();
@@ -19,30 +21,43 @@ const ResolutionsForDoctor = () => {
       limit: resolutionsOnPage,
     }));
   }, [dispatch]);
-  const { resolutions: myResolutions } = useAppSelector(selectResolutions);
+  const { resolutions: myResolutions, status: responseStatus } = useAppSelector(selectResolutions);
   return (
     <div>
       <DoctorNavigatePanel pageTitle={dictionary.doctorPage.resolutionsTitle} />
-      <ResolutionsTable>
-        <thead>
-          <ResolutionsTableHead>
-            {Object.entries(columnsNames)
-              .map(([key, value]) => <ResolutionsTableHeaderCell as="th" key={key}>{value}</ResolutionsTableHeaderCell>)}
-          </ResolutionsTableHead>
-        </thead>
-        <tbody>
-          {myResolutions.map((resolution) => (
-            <ResolutionRow
-              key={resolution.id}
-              firstName={resolution.patient.first_name}
-              lastName={resolution.patient.last_name}
-              resolution={resolution.resolution}
-              visitDate={format(new Date(resolution.visit_date), visitDate)}
-              nextAppointmentDate={format(new Date(resolution.next_appointment_date), visitDate)}
-            />
-          ))}
-        </tbody>
-      </ResolutionsTable>
+      {responseStatus !== 'loading'
+        ? (
+          <ResolutionsTable>
+            <thead>
+              <ResolutionsTableHead>
+                {Object.entries(columnsNames)
+                  .map(([key, value]) => <ResolutionsTableHeaderCell as="th" key={key}>{value}</ResolutionsTableHeaderCell>)}
+              </ResolutionsTableHead>
+            </thead>
+            <tbody>
+              {myResolutions.map((resolution) => (
+                <ResolutionRow
+                  key={resolution.id}
+                  firstName={resolution.patient.first_name}
+                  lastName={resolution.patient.last_name}
+                  resolution={resolution.resolution}
+                  visitDate={format(new Date(resolution.visit_date), visitDate)}
+                  nextAppointmentDate={format(new Date(resolution.next_appointment_date), visitDate)}
+                />
+              ))}
+            </tbody>
+          </ResolutionsTable>
+        )
+        : (
+          <Loader
+            type="MutatingDots"
+            color={colors.cornflower_blue}
+            secondaryColor={colors.radical_red}
+            timeout={5000}
+            height={150}
+            width={150}
+          />
+        )}
       {myResolutions.length > 0 && <ResolutionsPaginate />}
     </div>
   );
