b0c3c795ad50ad4eaea041229e01818ca7f40c29
victoria.buyakova <lineviktoriya@gmail.com>
2022-01-28 15:13:59 +0300

    refactor: move return data in cardsForDoctor.saga to separate function

--- src/redux/sagas/cardsForDoctor.saga.ts
+++ src/redux/sagas/cardsForDoctor.saga.ts
@@ -7,24 +7,28 @@ import appointments from '../../resources/appointments/appointments.api';
 import { ResolutionsResponse } from '../../resources/resolutions/resolutions.types';
 import resolutionsAPI from '../../resources/resolutions/resolutions.api';
 
+function createCardsForDoctorState(appointmentsResponse: AxiosResponse<AppointmentsForDoctor>, resolutionResponse: AxiosResponse<ResolutionsResponse>) {
+  return {
+    cards: [...appointmentsResponse.data.appointments.map((appointment) => ({
+      visitDate: appointment.visit_date,
+      firstName: appointment.patient.first_name,
+      lastName: appointment.patient.last_name,
+      photo: appointment.patient.photo,
+      appointmentID: appointment.id,
+      status: appointment.status,
+      resolution: resolutionResponse.data.resolutions.find((resolution) => (resolution.appointment_id === appointment.id)),
+    })),
+    ],
+    total: appointmentsResponse.data.total,
+  };
+}
+
 function* cardsForDoctorGet(action: ReturnType<typeof cardsForDoctor.pending>) {
   try {
     const { payload } = action;
     const appointmentsResponse: AxiosResponse<AppointmentsForDoctor> = yield call(appointments.fetchAppointmentsForDoctor, payload.offset, payload.limit);
     const resolutionResponse: AxiosResponse<ResolutionsResponse> = yield call(resolutionsAPI.fetchResolutions, payload.offset, payload.limit);
-    return {
-      cards: [...appointmentsResponse.data.appointments.map((appointment) => ({
-        visitDate: appointment.visit_date,
-        firstName: appointment.patient.first_name,
-        lastName: appointment.patient.last_name,
-        photo: appointment.patient.photo,
-        appointmentID: appointment.id,
-        status: appointment.status,
-        resolution: resolutionResponse.data.resolutions.find((resolution) => (resolution.appointment_id === appointment.id)),
-      })),
-      ],
-      total: appointmentsResponse.data.total,
-    };
+    return createCardsForDoctorState(appointmentsResponse, resolutionResponse);
   } catch (error:any) {
     yield put(notificationError(createErrorNotificationMessage(error.response.data)));
     throw error;
