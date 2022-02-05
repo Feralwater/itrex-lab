eb5a863bc3ab37b29925b0b8cae8d3902cfc4a29
victoria.buyakova <lineviktoriya@gmail.com>
2022-02-06 00:30:21 +0300

    refactor: remove middleware from makeAppointment.saga

--- src/redux/sagas/makeAppointment.saga.ts
+++ src/redux/sagas/makeAppointment.saga.ts
@@ -2,26 +2,23 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 import { AxiosResponse } from 'axios';
 import { NewAppointmentResponse } from '../../resources/appointments/appointments.types';
 import appointments from '../../resources/appointments/appointments.api';
-import { createErrorNotificationMessage, utils } from './utils';
+import { createErrorNotificationMessage } from './utils';
 import { componentsDictionary } from '../../components';
 import { makeAppointmentSlice, notificationSlice } from '../reducers';
 
-function* appointmentPost(action: ReturnType<typeof makeAppointmentSlice.actions.pending>) {
+function* appointmentPost({ payload }: ReturnType<typeof makeAppointmentSlice.actions.pending>) {
   try {
-    const { payload } = action;
-    const response: AxiosResponse<NewAppointmentResponse> = yield call(appointments.addAppointment, { ...payload });
+    const { data }: AxiosResponse<NewAppointmentResponse> = yield call(appointments.addAppointment, { ...payload });
     yield put(notificationSlice.actions.notificationSuccess(componentsDictionary.message.successMessageBodyMakeAppointment));
-    return response.data;
+    yield put(makeAppointmentSlice.actions.fulfilled(data));
   } catch (error:any) {
     yield put(notificationSlice.actions.notificationError(createErrorNotificationMessage(error.response.data)));
-    throw error;
+    yield put(makeAppointmentSlice.actions.failed());
   }
 }
 
-const appointmentPostSaga = utils.bind(null, makeAppointmentSlice.actions, appointmentPost);
-
 function* appointmentPostWatcher() {
-  yield takeEvery(makeAppointmentSlice.actions.pending, appointmentPostSaga);
+  yield takeEvery(makeAppointmentSlice.actions.pending, appointmentPost);
 }
 
 function* makeAppointmentSaga() {
