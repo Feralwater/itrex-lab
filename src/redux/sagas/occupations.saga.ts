b172eb7b0c966768f5a8493c2727f1901cf0d728
victoria.buyakova <lineviktoriya@gmail.com>
2022-02-06 00:36:05 +0300

    refactor: remove middleware from occupations.saga

--- src/redux/sagas/occupations.saga.ts
+++ src/redux/sagas/occupations.saga.ts
@@ -1,24 +1,22 @@
 import { AxiosResponse } from 'axios';
 import { call, put, takeEvery } from 'redux-saga/effects';
-import { createErrorNotificationMessage, utils } from './utils';
+import { createErrorNotificationMessage } from './utils';
 import occupationsAPI from '../../resources/occupations/occupations.api';
 import { OccupationsResponse } from '../../resources/occupations/occupations.types';
 import { notificationSlice, occupationsSlice } from '../reducers';
 
 function* getOccupations() {
   try {
-    const response: AxiosResponse<OccupationsResponse> = yield call(occupationsAPI.fetchOccupations);
-    return response.data;
+    const { data }: AxiosResponse<OccupationsResponse> = yield call(occupationsAPI.fetchOccupations);
+    yield put(occupationsSlice.actions.fulfilled(data));
   } catch (error:any) {
     yield put(notificationSlice.actions.notificationError(createErrorNotificationMessage(error.response.data)));
-    throw error;
+    yield put(occupationsSlice.actions.failed());
   }
 }
 
-const getOccupationsSaga = utils.bind(null, occupationsSlice.actions, getOccupations);
-
 function* getOccupationsWatcher() {
-  yield takeEvery(occupationsSlice.actions.pending, getOccupationsSaga);
+  yield takeEvery(occupationsSlice.actions.pending, getOccupations);
 }
 
 function* occupationsSaga() {
