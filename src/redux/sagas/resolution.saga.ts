c940b41be57befe7e38a449f0b7570895fccafd0
victoria.buyakova <lineviktoriya@gmail.com>
2022-02-06 01:04:00 +0300

    refactor: remove middleware from resolution.saga

--- src/redux/sagas/resolution.saga.ts
+++ src/redux/sagas/resolution.saga.ts
@@ -2,26 +2,23 @@ import { AxiosResponse } from 'axios';
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { ResolutionResponse } from '../../resources/resolutions/resolutions.types';
 import resolutionsAPI from '../../resources/resolutions/resolutions.api';
-import { createErrorNotificationMessage, utils } from './utils';
+import { createErrorNotificationMessage } from './utils';
 import { componentsDictionary } from '../../components';
 import { notificationSlice, resolutionSlice } from '../reducers';
 
-function* resolutionPost(action: ReturnType<typeof resolutionSlice.actions.pending>) {
+function* resolutionPost({ payload }: ReturnType<typeof resolutionSlice.actions.pending>) {
   try {
-    const { payload } = action;
-    const response: AxiosResponse<ResolutionResponse> = yield call(resolutionsAPI.createResolution, { ...payload });
+    const { data }: AxiosResponse<ResolutionResponse> = yield call(resolutionsAPI.createResolution, { ...payload });
     yield put(notificationSlice.actions.notificationSuccess(componentsDictionary.message.successMessageBodyCreateResolution));
-    return response.data;
+    yield put(resolutionSlice.actions.fulfilled(data));
   } catch (error:any) {
     yield put(notificationSlice.actions.notificationError(createErrorNotificationMessage(error.response.data)));
-    throw error;
+    yield put(resolutionSlice.actions.failed());
   }
 }
 
-const resolutionPostSaga = utils.bind(null, resolutionSlice.actions, resolutionPost);
-
 function* resolutionPostWatcher() {
-  yield takeEvery(resolutionSlice.actions.pending, resolutionPostSaga);
+  yield takeEvery(resolutionSlice.actions.pending, resolutionPost);
 }
 
 function* resolutionSaga() {
