98ee82a9dcee1527742e1193c021441e7d740833
victoria.buyakova <lineviktoriya@gmail.com>
2022-02-07 15:44:56 +0300

    refactor: simplify & rename appointments sagas

--- src/redux/sagas/rootSaga.ts
+++ src/redux/sagas/rootSaga.ts
@@ -1,29 +1,21 @@
 import { all } from 'redux-saga/effects';
 import loginSaga from './login.saga';
-import makeAppointmentSaga from './makeAppointment.saga';
+import { appointmentWatcher } from './appointment.saga';
 import registrationSaga from './registration.saga';
 import { profileWatcher } from './profile.saga';
-import deleteAppointmentSaga from './deleteAppointment.saga';
 import { resolutionWatcher } from './resolution.saga';
 import { fetchResolutionsWatcher } from './fetchResolutions.saga';
-import occupationsSaga from './occupations.saga';
-import getDoctorsByIDSaga from './getDoctorsByID.saga';
-import freeDoctorTimeSaga from './freeTime.saga';
 import { fetchAppointmentsWatcher } from './fetchAppointments.saga';
 
 function* rootSaga() {
   yield all([
     loginSaga(),
-    makeAppointmentSaga(),
     registrationSaga(),
-    deleteAppointmentSaga(),
-    occupationsSaga(),
-    getDoctorsByIDSaga(),
-    freeDoctorTimeSaga(),
     fetchAppointmentsWatcher(),
     fetchResolutionsWatcher(),
     resolutionWatcher(),
     profileWatcher(),
+    appointmentWatcher(),
   ]);
 }
 
