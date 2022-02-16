260c45479b964022d69243301ce7ffdb110d59e1
victoria.buyakova <lineviktoriya@gmail.com>
2022-02-16 18:08:40 +0300

    refactor: simplify skeleton array fill logic

--- src/components/Skeleton/SkeletonCards.tsx
+++ src/components/Skeleton/SkeletonCards.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { v1 } from 'uuid';
+import { appointmentsPerPage } from 'modules/hooks/constants';
 import { SkeletonCard } from './SkeletonCard';
 import { SkeletonBody, SkeletonCardContainer } from './Skeleton.styles';
 
 export const SkeletonCards:React.VFC = () => (
   <SkeletonBody>
-    {Array(12).fill(<SkeletonCard />).map((card) => <SkeletonCardContainer key={v1()}>{card}</SkeletonCardContainer>)}
+    {[...Array(appointmentsPerPage)].map(() => <SkeletonCardContainer key={v1()}><SkeletonCard /></SkeletonCardContainer>)}
   </SkeletonBody>
 );
