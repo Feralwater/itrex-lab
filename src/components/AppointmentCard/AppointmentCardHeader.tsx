57e24231d4c2a96c5e320098200706c0334ef7cf
victoria.buyakova <lineviktoriya@gmail.com>
2022-02-20 23:31:30 +0300

    separate change status modal window

--- src/components/AppointmentCard/AppointmentCardHeader.tsx
+++ src/components/AppointmentCard/AppointmentCardHeader.tsx
@@ -1,4 +1,6 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, {
+  ChangeEvent, Dispatch, SetStateAction, useRef, useState,
+} from 'react';
 import {
   AppointmentStatus, AppointmentStatusSwitcher, AppointmentSubTitle,
   DoctorSpecializationName, Status, StatusList,
@@ -16,100 +18,118 @@ import {
 import { componentsDictionary } from 'components/dictionary/componentsDictionary';
 import { useAppSelector } from 'hooks';
 
+interface ChangeStatusModalProps{
+  appointmentID:string
+  setOpenModalWindow: Dispatch<SetStateAction<boolean>>;
+}
+
+export const ChangeStatusModal:React.VFC<ChangeStatusModalProps> = (
+  { appointmentID, setOpenModalWindow },
+) => {
+  const [activeAppointmentStatusModal, setActiveAppointmentStatusModal] = useState<boolean>(true);
+  const [activeStatus, setActiveStatus] = useState<string>(statuses.waiting);
+  const { appointments } = useAppSelector(selectAppointmentsForDoctor);
+  const selectedAppointment = appointments.find((appointment) => appointment.appointmentID === appointmentID);
+  function changeAppointmentStatus(event: ChangeEvent<HTMLInputElement>) {
+    setActiveStatus(event.currentTarget.name);
+  }
+  const saveAppointmentStatusHandler = () => {
+    setActiveAppointmentStatusModal(false);
+    setOpenModalWindow(false);
+  };
+  const cancelHandler = () => {
+    setActiveAppointmentStatusModal(false);
+    setOpenModalWindow(false);
+  };
+  return (
+    <ModalWindow
+      activeModal={activeAppointmentStatusModal}
+      setActiveModal={setActiveAppointmentStatusModal}
+    >
+      <ResolutionModalBody>
+        <ResolutionModalTitle>{componentsDictionary.changeAppointmentStatus.title}</ResolutionModalTitle>
+        <SelectedPatientInfo>
+          <SelectedPatientImage
+            src={selectedAppointment?.photo}
+            alt={componentsDictionary.controlCardPanel.selectedPatientImageAlt}
+          />
+          <span>
+            {selectedAppointment?.firstName}
+            {' '}
+            {selectedAppointment?.lastName}
+          </span>
+        </SelectedPatientInfo>
+        {componentsDictionary.changeAppointmentStatus.subTitle}
+        <AppointmentStatusSwitcher>
+          <StatusList>
+            <Status>
+              <label htmlFor={statuses.waiting}>
+                <input
+                  type="radio"
+                  name={statuses.waiting}
+                  checked={activeStatus === statuses.waiting}
+                  onChange={changeAppointmentStatus}
+                />
+                {statusDescription[statuses.waiting]}
+              </label>
+            </Status>
+            <Status>
+              <label htmlFor={statuses.confirmed}>
+                <input
+                  type="radio"
+                  name={statuses.confirmed}
+                  checked={activeStatus === statuses.confirmed}
+                  onChange={changeAppointmentStatus}
+                />
+                {statusDescription[statuses.confirmed]}
+              </label>
+            </Status>
+            <Status>
+              <label htmlFor={statuses.canceled}>
+                <input
+                  type="radio"
+                  name={statuses.canceled}
+                  checked={activeStatus === statuses.canceled}
+                  onChange={changeAppointmentStatus}
+                />
+                {statusDescription[statuses.canceled]}
+              </label>
+            </Status>
+          </StatusList>
+        </AppointmentStatusSwitcher>
+      </ResolutionModalBody>
+      <ResolutionModalButtons
+        activeButtonType="button"
+        cancelHandler={cancelHandler}
+        saveHandler={saveAppointmentStatusHandler}
+        passiveButtonText={dictionary.resolutionModal.cancelButtonText}
+        activeButtonText={dictionary.resolutionModal.saveButtonText}
+        activeButtonIcon="/svg/save-icon.svg"
+        passiveButtonIcon="/svg/close-icon.svg"
+      />
+    </ModalWindow>
+  );
+};
+
 export const AppointmentCardHeader:React.VFC<AppointmentCardHeaderProps> = ({
   status,
   specialization,
   appointmentID,
 }) => {
-  const [activeAppointmentStatusModal, setActiveAppointmentStatusModal] = useState<boolean>(true);
-  const [activeStatus, setActiveStatus] = useState<string>('waiting');
-  const { appointments } = useAppSelector(selectAppointmentsForDoctor);
-  const selectedAppointment = appointments.find((appointment) => appointment.appointmentID === appointmentID);
-  function changeValue(event: ChangeEvent<HTMLInputElement>) {
-    setActiveStatus(event.currentTarget.name);
-  }
-  const saveAppointmentStatusHandler = () => { };
-  const cancelHandler = () => {
-    setActiveAppointmentStatusModal(false);
-  };
+  const [openModalWindow, setOpenModalWindow] = useState<boolean>(false);
   return (
-    <AppointmentSubTitle>
-      {status
-        ? (
-          <>
-            <AppointmentStatus>
+    <>
+      <AppointmentSubTitle>
+        {status
+          ? (
+            <AppointmentStatus onClick={() => setOpenModalWindow(true)}>
               <UserCardBodyAppointmentConfirm color={statusColor[status]} />
               <div>{statusDescription[status]}</div>
             </AppointmentStatus>
-            <ModalWindow
-              activeModal={activeAppointmentStatusModal}
-              setActiveModal={setActiveAppointmentStatusModal}
-            >
-              <ResolutionModalBody>
-                <ResolutionModalTitle>{componentsDictionary.changeAppointmentStatus.title}</ResolutionModalTitle>
-                <SelectedPatientInfo>
-                  <SelectedPatientImage
-                    src={selectedAppointment?.photo}
-                    alt={componentsDictionary.controlCardPanel.selectedPatientImageAlt}
-                  />
-                  <span>
-                    {selectedAppointment?.firstName}
-                    {' '}
-                    {selectedAppointment?.lastName}
-                  </span>
-                </SelectedPatientInfo>
-                {componentsDictionary.changeAppointmentStatus.subTitle}
-                <AppointmentStatusSwitcher>
-                  <StatusList>
-                    <Status>
-                      <label htmlFor={statuses.waiting}>
-                        <input
-                          type="radio"
-                          name={statuses.waiting}
-                          checked={activeStatus === statuses.waiting}
-                          onChange={changeValue}
-                        />
-                        {statusDescription[statuses.waiting]}
-                      </label>
-                    </Status>
-                    <Status>
-                      <label htmlFor={statuses.confirmed}>
-                        <input
-                          type="radio"
-                          name={statuses.confirmed}
-                          checked={activeStatus === statuses.confirmed}
-                          onChange={changeValue}
-                        />
-                        {statusDescription[statuses.confirmed]}
-                      </label>
-                    </Status>
-                    <Status>
-                      <label htmlFor={statuses.canceled}>
-                        <input
-                          type="radio"
-                          name={statuses.canceled}
-                          checked={activeStatus === statuses.canceled}
-                          onChange={changeValue}
-                        />
-                        {statusDescription[statuses.canceled]}
-                      </label>
-                    </Status>
-                  </StatusList>
-                </AppointmentStatusSwitcher>
-              </ResolutionModalBody>
-              <ResolutionModalButtons
-                activeButtonType="button"
-                cancelHandler={cancelHandler}
-                saveHandler={saveAppointmentStatusHandler}
-                passiveButtonText={dictionary.resolutionModal.cancelButtonText}
-                activeButtonText={dictionary.resolutionModal.saveButtonText}
-                activeButtonIcon="/svg/save-icon.svg"
-                passiveButtonIcon="/svg/close-icon.svg"
-              />
-            </ModalWindow>
-          </>
-        )
-        : <DoctorSpecializationName>{specialization}</DoctorSpecializationName>}
-    </AppointmentSubTitle>
+          )
+          : <DoctorSpecializationName>{specialization}</DoctorSpecializationName>}
+      </AppointmentSubTitle>
+      {openModalWindow && <ChangeStatusModal appointmentID={appointmentID} setOpenModalWindow={setOpenModalWindow} />}
+    </>
   );
 };
