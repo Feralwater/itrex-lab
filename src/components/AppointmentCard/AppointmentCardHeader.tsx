2bf175d3222cc7c258d893e5a671582a98b1be63
victoria.buyakova <lineviktoriya@gmail.com>
2022-02-20 20:29:33 +0300

    add modal window & radio buttons at AppointmentCardHeader

--- src/components/AppointmentCard/AppointmentCardHeader.tsx
+++ src/components/AppointmentCard/AppointmentCardHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import {
   AppointmentStatus, AppointmentSubTitle,
   DoctorSpecializationName,
@@ -6,19 +6,56 @@ import {
 } from 'components/AppointmentCard/AppointmentCard.styles';
 import { statusColor, statusDescription } from 'components/AppointmentCard/constants';
 import { AppointmentCardHeaderProps } from 'components/AppointmentCard/AppointmentCard.types';
+import { ModalWindow } from 'components/Modal';
 
 export const AppointmentCardHeader:React.VFC<AppointmentCardHeaderProps> = ({
   status,
   specialization,
-}) => (
-  <AppointmentSubTitle>
-    { status
-      ? (
-        <AppointmentStatus>
-          <UserCardBodyAppointmentConfirm color={statusColor[status]} />
-          <div>{statusDescription[status]}</div>
-        </AppointmentStatus>
-      )
-      : <DoctorSpecializationName>{specialization}</DoctorSpecializationName>}
-  </AppointmentSubTitle>
-);
+}) => {
+  const [activeAppointmentStatusModal, setActiveAppointmentStatusModal] = useState<boolean>(true);
+  const [activeStatus, setActiveStatus] = useState<string>('waiting');
+  function changeValue(event: ChangeEvent<HTMLInputElement>) {
+    setActiveStatus(event.currentTarget.name);
+  }
+  return (
+    <AppointmentSubTitle>
+      {status
+        ? (
+          <>
+            <AppointmentStatus>
+              <UserCardBodyAppointmentConfirm color={statusColor[status]} />
+              <div>{statusDescription[status]}</div>
+            </AppointmentStatus>
+            <ModalWindow
+              activeModal={activeAppointmentStatusModal}
+              setActiveModal={setActiveAppointmentStatusModal}
+            >
+              <div>
+                <input
+                  type="radio"
+                  name="confirmed"
+                  checked={activeStatus === 'confirmed'}
+                  onChange={changeValue}
+                />
+
+                <input
+                  type="radio"
+                  name="canceled"
+                  checked={activeStatus === 'canceled'}
+                  onChange={changeValue}
+                />
+
+                <input
+                  type="radio"
+                  name="waiting"
+                  checked={activeStatus === 'waiting'}
+                  onChange={changeValue}
+                />
+              </div>
+            </ModalWindow>
+          </>
+        )
+        : <DoctorSpecializationName>{specialization}</DoctorSpecializationName>}
+    </AppointmentSubTitle>
+  );
+};
