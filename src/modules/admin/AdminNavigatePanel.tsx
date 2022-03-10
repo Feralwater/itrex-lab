09b12137b82145e6a750b1de4e70314e972d4611
victoria.buyakova <lineviktoriya@gmail.com>
2022-03-10 12:05:02 +0300

    move create patient window to separate component

--- src/modules/admin/AdminNavigatePanel.tsx
+++ src/modules/admin/AdminNavigatePanel.tsx
@@ -1,27 +1,14 @@
-import { AdminNavigate, CreatePatientFields } from 'modules/admin/AdminPage.styles';
+import { AdminNavigate } from 'modules/admin/AdminPage.styles';
 import { NavigatePanel } from 'components/NavigatePanel/NavigatePanel';
 import { adminTabs } from 'modules/admin/constants';
-import {
-  Button, InputFormContainer, ModalWindow, ResolutionModalButtons,
-} from 'components';
-import React, { ChangeEvent, useState } from 'react';
-import { dictionary } from 'pages';
+import { Button } from 'components';
+import React, { useState } from 'react';
+import { CreatePatientModal } from 'modules/admin/CreatePatientModal';
 
 export const AdminNavigatePanel:React.FC = ({ children }) => {
   const [createPatientWindow, setCreatePatientWindow] = useState<boolean>(false);
-  const [firstName, setFirstName] = useState<string>('');
-  const [lastName, setLastName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const firstNameChangeHandler = (event:ChangeEvent<HTMLInputElement>) => setFirstName(event.currentTarget.value);
-  const lastNameChangeHandler = (event:ChangeEvent<HTMLInputElement>) => setLastName(event.currentTarget.value);
-  const emailChangeHandler = (event:ChangeEvent<HTMLInputElement>) => setEmail(event.currentTarget.value);
-  const passwordChangeHandler = (event:ChangeEvent<HTMLInputElement>) => setPassword(event.currentTarget.value);
   const openCreatePatientWindow = () => setCreatePatientWindow(true);
-  const cancelHandler = () => setCreatePatientWindow(false);
-  const createPatientHandle = () => {
-    setCreatePatientWindow(false);
-  };
+
   return (
     <>
       <AdminNavigate>
@@ -37,64 +24,7 @@ export const AdminNavigatePanel:React.FC = ({ children }) => {
           {children}
         </Button>
       </AdminNavigate>
-      <ModalWindow activeModal={createPatientWindow} setActiveModal={setCreatePatientWindow}>
-        <CreatePatientFields>
-          <InputFormContainer
-            inputSize="small"
-            icon="default"
-            id="patientFirstName"
-            type="text"
-            isRequire
-            placeholder={dictionary.userModal.firstNamePlaceholder}
-            value={firstName}
-            label={dictionary.userModal.firstNameLabel}
-            onChange={firstNameChangeHandler}
-          />
-          <InputFormContainer
-            inputSize="small"
-            icon="default"
-            id="lastName"
-            type="text"
-            isRequire
-            placeholder={dictionary.userModal.secondNamePlaceholder}
-            value={lastName}
-            label={dictionary.userModal.secondNameLabel}
-            onChange={lastNameChangeHandler}
-          />
-          <InputFormContainer
-            inputSize="small"
-            icon="default"
-            id="email"
-            type="email"
-            isRequire
-            placeholder={dictionary.userModal.emailPlaceholder}
-            value={email}
-            label={dictionary.userModal.emailLabel}
-            onChange={emailChangeHandler}
-          />
-          <InputFormContainer
-            inputSize="small"
-            icon="default"
-            id="password"
-            type="password"
-            isRequire
-            placeholder={dictionary.userModal.passwordPlaceholder}
-            value={password}
-            label={dictionary.userModal.passwordLabel}
-            onChange={passwordChangeHandler}
-          />
-        </CreatePatientFields>
-        <ResolutionModalButtons
-          disabled={false}
-          activeButtonType="button"
-          cancelHandler={cancelHandler}
-          saveHandler={createPatientHandle}
-          passiveButtonText={dictionary.userModal.cancelButtonText}
-          activeButtonText={dictionary.userModal.saveButtonText}
-          activeButtonIcon="/svg/board-icon.svg"
-          passiveButtonIcon="/svg/close-icon.svg"
-        />
-      </ModalWindow>
+      <CreatePatientModal createPatientWindow={createPatientWindow} setCreatePatientWindow={setCreatePatientWindow} />
     </>
   );
 };
---
+++ src/modules/admin/CreatePatientModal.tsx
@@ -0,0 +1,89 @@
+import React, {
+  ChangeEvent, Dispatch, SetStateAction, useState,
+} from 'react';
+import { InputFormContainer, ModalWindow, ResolutionModalButtons } from 'components';
+import { CreatePatientFields } from 'modules/admin/AdminPage.styles';
+import { dictionary } from 'pages';
+
+interface CreatePatientModalProps {
+  createPatientWindow: boolean
+  setCreatePatientWindow: Dispatch<SetStateAction<boolean>>;
+}
+
+export const CreatePatientModal: React.VFC<CreatePatientModalProps> = ({
+  createPatientWindow, setCreatePatientWindow,
+}) => {
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const firstNameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => setFirstName(event.currentTarget.value);
+  const lastNameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => setLastName(event.currentTarget.value);
+  const emailChangeHandler = (event: ChangeEvent<HTMLInputElement>) => setEmail(event.currentTarget.value);
+  const passwordChangeHandler = (event: ChangeEvent<HTMLInputElement>) => setPassword(event.currentTarget.value);
+
+  const cancelHandler = () => setCreatePatientWindow(false);
+  const createPatientHandle = () => {
+    setCreatePatientWindow(false);
+  };
+  return (
+    <ModalWindow activeModal={createPatientWindow} setActiveModal={setCreatePatientWindow}>
+      <CreatePatientFields>
+        <InputFormContainer
+          inputSize="small"
+          icon="default"
+          id="patientFirstName"
+          type="text"
+          isRequire
+          placeholder={dictionary.userModal.firstNamePlaceholder}
+          value={firstName}
+          label={dictionary.userModal.firstNameLabel}
+          onChange={firstNameChangeHandler}
+        />
+        <InputFormContainer
+          inputSize="small"
+          icon="default"
+          id="lastName"
+          type="text"
+          isRequire
+          placeholder={dictionary.userModal.secondNamePlaceholder}
+          value={lastName}
+          label={dictionary.userModal.secondNameLabel}
+          onChange={lastNameChangeHandler}
+        />
+        <InputFormContainer
+          inputSize="small"
+          icon="default"
+          id="email"
+          type="email"
+          isRequire
+          placeholder={dictionary.userModal.emailPlaceholder}
+          value={email}
+          label={dictionary.userModal.emailLabel}
+          onChange={emailChangeHandler}
+        />
+        <InputFormContainer
+          inputSize="small"
+          icon="default"
+          id="password"
+          type="password"
+          isRequire
+          placeholder={dictionary.userModal.passwordPlaceholder}
+          value={password}
+          label={dictionary.userModal.passwordLabel}
+          onChange={passwordChangeHandler}
+        />
+      </CreatePatientFields>
+      <ResolutionModalButtons
+        disabled={false}
+        activeButtonType="button"
+        cancelHandler={cancelHandler}
+        saveHandler={createPatientHandle}
+        passiveButtonText={dictionary.userModal.cancelButtonText}
+        activeButtonText={dictionary.userModal.saveButtonText}
+        activeButtonIcon="/svg/board-icon.svg"
+        passiveButtonIcon="/svg/close-icon.svg"
+      />
+    </ModalWindow>
+  );
+};
