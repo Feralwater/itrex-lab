7c503bc4d457d7fdb50b4ce65408bef233c94aeb
victoria.buyakova <lineviktoriya@gmail.com>
2022-03-27 21:43:12 +0300

    refactor modal window

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {
+  useCallback, useEffect, useImperativeHandle, useState,
+} from 'react';
+import { createPortal } from 'react-dom';
 import { ModalProps } from './Modal.types';
 import { Modal, ModalContent } from './Modal.styles';
 
-export const ModalWindow: React.FC<ModalProps> = (
-  { activeModal, setActiveModal, children },
-) => (
-  <Modal onClick={() => setActiveModal(false)} isActive={activeModal}>
-    <ModalContent onClick={((event) => event.stopPropagation())} isActive={activeModal}>
-      {children}
-    </ModalContent>
-  </Modal>
-);
+const modalElement = document.getElementById('modal-root');
+
+export const ModalWindow: React.FC<ModalProps> = ({ children, defaultOpened = false }, ref) => {
+  const [isOpen, setIsOpen] = useState<boolean>(defaultOpened);
+
+  const close = useCallback(() => setIsOpen(false), []);
+
+  useImperativeHandle(ref, () => ({
+    open: () => setIsOpen(true),
+    close,
+  }), [close]);
+
+  const handleEscape = useCallback((event) => {
+    if (event.keyCode === 27) close();
+  }, [close]);
+
+  useEffect(() => {
+    if (isOpen) document.addEventListener('keydown', handleEscape, false);
+    return () => {
+      document.removeEventListener('keydown', handleEscape, false);
+    };
+  }, [handleEscape, isOpen]);
+
+  return createPortal(isOpen
+    ? (
+      <Modal>
+        <ModalContent>
+          {children}
+        </ModalContent>
+      </Modal>
+    ) : null, modalElement);
+};
